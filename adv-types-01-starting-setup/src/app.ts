3eb0e840cef58692b96ff0c2162415baa4430986
Abdurahim <nurmatov.abdurahim98@mail.ru>
2022-09-02 01:52:58 +0500

    type guards

--- adv-types-01-starting-setup/src/app.ts
+++ adv-types-01-starting-setup/src/app.ts
@@ -23,3 +23,48 @@ type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
 
+function add(n1: Combinable, n2: Combinable) {
+  if (typeof n1 === "string" || typeof n2 === "string") {
+    return n1.toString() + n2.toString();
+  }
+  return n1 + n2;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+}
+
+printEmployeeInformation({ name: "John", privileges: ["do it now"] });
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving...");
+  }
+
+  loadCargo() {
+    console.log("Loading cargo...");
+  }
+}
+
+type Vehicle = Car | Truck;
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo();
+  }
+}
+
+useVehicle(new Car());
+useVehicle(new Truck());
