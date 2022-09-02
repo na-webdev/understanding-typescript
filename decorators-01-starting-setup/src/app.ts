bca7d216ac17cbf61abb8aead7efe1f9f7a393c8
Abdurahim <nurmatov.abdurahim98@mail.ru>
2022-09-03 03:21:18 +0500

    validator decorators

--- decorators-01-starting-setup/src/app.ts
+++ decorators-01-starting-setup/src/app.ts
@@ -116,3 +116,79 @@ const newPrinter = new Printer();
 
 const button = document.getElementById("button")!;
 button.addEventListener("click", newPrinter.showMessage);
+
+interface ValidatorConfig {
+  [property: string]: {
+    [validatableProp: string]: string[];
+  };
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      "required",
+    ],
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      "positive",
+    ],
+  };
+}
+
+function validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) {
+    return true;
+  }
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case "required":
+          isValid = isValid && !!obj[prop];
+          break;
+        case "positive":
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+  return isValid;
+}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseForm = document.getElementById("form")! as HTMLFormElement;
+
+courseForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const title = courseForm.courseName.value as string;
+  const price = +courseForm.price.value;
+
+  const createdCourse = new Course(title, price);
+  if (!validate(createdCourse)) {
+    alert("Entered data is invalid. Please try again!");
+    return;
+  }
+  console.log(createdCourse);
+});
